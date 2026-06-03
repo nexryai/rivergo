979a3453018d946c2edead0ab404d0c4a5bf857e
nexryai <nexryai@sda1.net>
2026-06-03 12:56:10 +0000

    Improve types

--- yahoo.go
+++ yahoo.go
@@ -6,11 +6,106 @@ import (
 	"github.com/antchfx/htmlquery"
 )
 
+type Trend int
+
+const (
+	TrendUnknown Trend = iota
+	TrendUp
+	TrendDown
+	TrendUnchange
+	TrendMissing
+)
+
+func (t Trend) String() string {
+	switch t {
+	case TrendUp:
+		return "waterLevelUp"
+	case TrendDown:
+		return "waterLevelDown"
+	case TrendUnchange:
+		return "waterLevelUnchange"
+	case TrendMissing:
+		return "waterLevelMissing"
+	default:
+		return "unknown"
+	}
+}
+
+func (t Trend) Description() string {
+	switch t {
+	case TrendUp:
+		return "上昇"
+	case TrendDown:
+		return "下降"
+	case TrendUnchange:
+		return "変化なし"
+	case TrendMissing:
+		return "欠測"
+	default:
+		return "不明"
+	}
+}
+
+type Condition int
+
+const (
+	ConditionUnknown Condition = iota
+	ConditionMissing
+	ConditionNone
+	ConditionNormal
+	ConditionFloodStandBy
+	ConditionFloodAdvisory
+	ConditionFloodEvacuation
+	ConditionFloodHazard
+)
+
+func (c Condition) String() string {
+	switch c {
+	case ConditionMissing:
+		return "levelMissing"
+	case ConditionNone:
+		return "levelNone"
+	case ConditionNormal:
+		return "levelNormal"
+	case ConditionFloodStandBy:
+		return "floodStandBy"
+	case ConditionFloodAdvisory:
+		return "floodAdvisory"
+	case ConditionFloodEvacuation:
+		return "floodEvacuation"
+	case ConditionFloodHazard:
+		return "floodHazard"
+	default:
+		return "unknown"
+	}
+}
+
+func (c Condition) Description() string {
+	switch c {
+	case ConditionMissing:
+		return "欠測"
+	case ConditionNone:
+		return "基準未設定"
+	case ConditionNormal:
+		return "平常"
+	case ConditionFloodStandBy:
+		return "水防団待機水位"
+	case ConditionFloodAdvisory:
+		return "氾濫注意水位"
+	case ConditionFloodEvacuation:
+		return "避難判断水位"
+	case ConditionFloodHazard:
+		return "氾濫危険水位"
+	default:
+		return "不明"
+	}
+}
+
 type Station struct {
 	ID        string
 	Name      string
-	Trend     string
-	Condition string
+	Trend     Trend
+	Condition Condition
 }
 
 func parseWaterLevelTable(htmlStr string) ([]Station, error) {
@@ -19,14 +114,14 @@ func parseWaterLevelTable(htmlStr string) ([]Station, error) {
 		return nil, err
 	}
 
-	conditionMap := map[string]string{
-		"levelMissing":    "欠測",
-		"levelNone":       "基準未設定",
-		"levelNormal":     "平常",
-		"floodStandBy":    "水防団待機水位",
-		"floodAdvisory":   "氾濫注意水位",
-		"floodEvacuation": "避難判断水位",
-		"floodHazard":     "氾濫危険水位",
+	conditionMap := map[string]Condition{
+		"levelMissing":     ConditionMissing,
+		"levelNone":        ConditionNone,
+		"levelNormal":      ConditionNormal,
+		"floodStandBy":     ConditionFloodStandBy,
+		"floodAdvisory":    ConditionFloodAdvisory,
+		"floodEvacuation":  ConditionFloodEvacuation,
+		"floodHazard":      ConditionFloodHazard,
 	}
 
 	nodes, err := htmlquery.QueryAll(doc, "//table[@id='waterLevelStationList']//tr[contains(@class, 'largeLine')]")
@@ -38,36 +133,34 @@ func parseWaterLevelTable(htmlStr string) ([]Station, error) {
 	for _, node := range nodes {
 		var s Station
 
-		// 観測所名の取得
 		if nameNode := htmlquery.FindOne(node, ".//span[@class='name']"); nameNode != nil {
 			s.Name = htmlquery.InnerText(nameNode)
 		}
 
-		// 観測所IDの取得
 		if linkNode := htmlquery.FindOne(node, ".//a[@class='stationLink']"); linkNode != nil {
 			s.ID = htmlquery.SelectAttr(linkNode, "data-obsrvtn")
 		}
 
-		// trendIconのsrcから水位の傾向を判断
 		if imgNode := htmlquery.FindOne(node, ".//span[@class='trendIcon']/img"); imgNode != nil {
 			src := htmlquery.SelectAttr(imgNode, "src")
 			switch {
 			case strings.Contains(src, "waterLevelUp"):
-				s.Trend = "上昇"
+				s.Trend = TrendUp
 			case strings.Contains(src, "waterLevelDown"):
-				s.Trend = "下降"
+				s.Trend = TrendDown
+			case strings.Contains(src, "waterLevelUnchange"):
+				s.Trend = TrendUnchange
 			case strings.Contains(src, "waterLevelMissing"):
-				s.Trend = "欠測"
+				s.Trend = TrendMissing
 			default:
-				s.Trend = "不明"
+				s.Trend = TrendUnknown
 			}
 		}
 
-		// クラス名から水位状況を判断
 		classAttr := htmlquery.SelectAttr(node, "class")
 		for _, class := range strings.Fields(classAttr) {
-			if cond, exists := conditionMap[class]; exists {
-				s.Condition = cond
+			if c, exists := conditionMap[class]; exists {
+				s.Condition = c
 				break
 			}
 		}
--- yahoo_test.go
+++ yahoo_test.go
@@ -5,24 +5,21 @@ import (
 )
 
 func TestParseWaterLevelTable(t *testing.T) {
-	htmlData := `<table class="waterLevelStationList target_modules" id="waterLevelStationList">
+	tests := []struct {
+		name     string
+		htmlData string
+		expected []Station
+	}{
+		{
+			name: "パターン1：氾濫注意",
+			htmlData: `<table class="waterLevelStationList target_modules" id="waterLevelStationList">
       <tbody>
-      <tr class="waterLevelStationListHeader">
-        <th class="stationNameHeader"><span>観測所名</span></th>
-        <th class="waterLevelHeader"><span>水位状況</span></th>
-      </tr>
-      <tr class="riverUpper">
-        <td colspan="2">
-          <span class="publishedDate">2026/6/3 20:30更新</span>
-          <span class="riverUpperLabel">上流</span>
-        </td>
-      </tr>
       <tr class="largeLine levelNormal">
         <td class="stationName"><span class="name">間黒</span></td>
         <td>
           <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0204900400078">
             <div class="waterLevel">
-              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png" width="20" height="20"></span>
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png"></span>
               <span class="waterLevelLabel">平常</span>
             </div>
           </a>
@@ -33,7 +30,7 @@ func TestParseWaterLevelTable(t *testing.T) {
         <td>
           <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0204900400079">
             <div class="waterLevel">
-              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png" width="20" height="20"></span>
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png"></span>
               <span class="waterLevelLabel">平常</span>
             </div>
           </a>
@@ -44,7 +41,7 @@ func TestParseWaterLevelTable(t *testing.T) {
         <td>
           <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0204900400106">
             <div class="waterLevel">
-              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelMissing_l.png" width="20" height="20"></span>
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelMissing_l.png"></span>
               <span class="waterLevelLabel">欠測</span>
             </div>
           </a>
@@ -55,7 +52,7 @@ func TestParseWaterLevelTable(t *testing.T) {
         <td>
           <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0204900400001">
             <div class="waterLevel">
-              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_black_l_02.png" width="20" height="20"></span>
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_black_l_02.png"></span>
               <span class="waterLevelLabel">氾濫注意水位</span>
             </div>
           </a>
@@ -66,41 +63,172 @@ func TestParseWaterLevelTable(t *testing.T) {
         <td>
           <a class="stationLink" href="javascript:void(0);" data-obsrvtn="2127100400023">
             <div class="waterLevel">
-              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUp_black_l_02.png" width="20" height="20"></span>
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUp_black_l_02.png"></span>
               <span class="waterLevelLabel">上昇中</span>
             </div>
           </a>
         </td>
       </tr>
-      <tr class="riverDown">
-        <td colspan="2">下流</td>
+      </tbody>
+    </table>`,
+			expected: []Station{
+				{ID: "0204900400078", Name: "間黒", Trend: TrendDown, Condition: ConditionNormal},
+				{ID: "0204900400079", Name: "加賀田", Trend: TrendDown, Condition: ConditionNormal},
+				{ID: "0204900400106", Name: "川根橋", Trend: TrendMissing, Condition: ConditionMissing},
+				{ID: "0204900400001", Name: "高橋", Trend: TrendDown, Condition: ConditionFloodAdvisory},
+				{ID: "2127100400023", Name: "下石崎", Trend: TrendUp, Condition: ConditionNone},
+			},
+		},
+		{
+			name: "パターン2：変化なし（waterLevelUnchange）を含む",
+			htmlData: `<table class="waterLevelStationList target_modules" id="waterLevelStationList">
+      <tbody>
+      <tr class="largeLine levelMissing">
+        <td class="stationName"><span class="name">桂川城南橋</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0486500400042">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelMissing_l.png"></span>
+              <span class="waterLevelLabel">欠測</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNormal">
+        <td class="stationName"><span class="name">桂川強瀬</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0486500400049">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png"></span>
+              <span class="waterLevelLabel">平常</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNone">
+        <td class="stationName"><span class="name">大月</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0358500400001">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUnchange_black_l.png"></span>
+              <span class="waterLevelLabel">変化なし</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNormal">
+        <td class="stationName"><span class="name">上依知</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0358500400002">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUnchange_white_l.png"></span>
+              <span class="waterLevelLabel">平常</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNormal">
+        <td class="stationName"><span class="name">磯部</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0358500400003">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUnchange_white_l.png"></span>
+              <span class="waterLevelLabel">平常</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNormal">
+        <td class="stationName"><span class="name">相模大橋</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="2132000400033">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png"></span>
+              <span class="waterLevelLabel">平常</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNormal">
+        <td class="stationName"><span class="name">厚木</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="0358500400004">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUnchange_white_l.png"></span>
+              <span class="waterLevelLabel">平常</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNormal">
+        <td class="stationName"><span class="name">神川橋</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="2132000400032">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_white_l_02.png"></span>
+              <span class="waterLevelLabel">平常</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNone">
+        <td class="stationName"><span class="name">神川橋（下）</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="2132000400212">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelDown_black_l_02.png"></span>
+              <span class="waterLevelLabel">下降中</span>
+            </div>
+          </a>
+        </td>
+      </tr>
+      <tr class="largeLine levelNone">
+        <td class="stationName"><span class="name">馬入橋</span></td>
+        <td>
+          <a class="stationLink" href="javascript:void(0);" data-obsrvtn="2132000400034">
+            <div class="waterLevel">
+              <span class="trendIcon"><img src="//s.yimg.jp/images/weather/smp/v2/img/river/detail_stationList_ico_waterLevelUp_black_l_02.png"></span>
+              <span class="waterLevelLabel">上昇中</span>
+            </div>
+          </a>
+        </td>
       </tr>
       </tbody>
-    </table>`
-
-	expected := []Station{
-		{ID: "0204900400078", Name: "間黒", Trend: "下降", Condition: "平常"},
-		{ID: "0204900400079", Name: "加賀田", Trend: "下降", Condition: "平常"},
-		{ID: "0204900400106", Name: "川根橋", Trend: "欠測", Condition: "欠測"},
-		{ID: "0204900400001", Name: "高橋", Trend: "下降", Condition: "氾濫注意水位"},
-		{ID: "2127100400023", Name: "下石崎", Trend: "上昇", Condition: "基準未設定"},
+    </table>`,
+			expected: []Station{
+				{ID: "0486500400042", Name: "桂川城南橋", Trend: TrendMissing, Condition: ConditionMissing},
+				{ID: "0486500400049", Name: "桂川強瀬", Trend: TrendDown, Condition: ConditionNormal},
+				{ID: "0358500400001", Name: "大月", Trend: TrendUnchange, Condition: ConditionNone},
+				{ID: "0358500400002", Name: "上依知", Trend: TrendUnchange, Condition: ConditionNormal},
+				{ID: "0358500400003", Name: "磯部", Trend: TrendUnchange, Condition: ConditionNormal},
+				{ID: "2132000400033", Name: "相模大橋", Trend: TrendDown, Condition: ConditionNormal},
+				{ID: "0358500400004", Name: "厚木", Trend: TrendUnchange, Condition: ConditionNormal},
+				{ID: "2132000400032", Name: "神川橋", Trend: TrendDown, Condition: ConditionNormal},
+				{ID: "2132000400212", Name: "神川橋（下）", Trend: TrendDown, Condition: ConditionNone},
+				{ID: "2132000400034", Name: "馬入橋", Trend: TrendUp, Condition: ConditionNone},
+			},
+		},
 	}
 
-	stations, err := parseWaterLevelTable(htmlData)
-	if err != nil {
-		t.Fatalf("ParseWaterLevelTable() failed: %v", err)
-	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stations, err := parseWaterLevelTable(tt.htmlData)
+			if err != nil {
+				t.Fatalf("ParseWaterLevelTable() failed: %v", err)
+			}
 
-	if len(stations) != len(expected) {
-		t.Fatalf("got %d stations, want %d", len(stations), len(expected))
-	}
+			if len(stations) != len(tt.expected) {
+				t.Fatalf("got %d stations, want %d", len(stations), len(tt.expected))
+			}
 
-	for i, want := range expected {
-		got := stations[i]
-		if got != want {
-			t.Errorf("stations[%d] = %+v; want %+v", i, got, want)
-		} else {
-			t.Logf("stations[%d] passed: ID=%s, Name=%s, Trend=%s, Condition=%s", i, got.ID, got.Name, got.Trend, got.Condition)
-		}
+			for i, want := range tt.expected {
+				got := stations[i]
+				if got != want {
+					t.Errorf("stations[%d] = %+v; want %+v", i, got, want)
+				} else {
+					t.Logf("stations[%d] passed: ID=%s, Name=%s, Trend=%v, Condition=%v", i, got.ID, got.Name, got.Trend, got.Condition)
+				}
+			}
+		})
 	}
 }
